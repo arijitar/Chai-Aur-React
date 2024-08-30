b9b4007253730bdfdb9fb32bdc730525a2d51ba9
arijitar <arijit.roy155k@gmail.com>
2024-08-30 20:20:20 +0530

    project on counter

---
+++ 03counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+//  let counter = 15
+ const addValue = ()=>{
+    // counter = counter + 1
+    if(counter < 20){
+      setCounter(counter + 1 )
+    }
+    console.log("clicked", counter);
+  
+ }
+ const removeValue = ()=>{
+  //  counter = counter - 1
+   if(counter>0){
+   setCounter(counter - 1)
+   }
+   console.log("clicked", counter);
+ }
+  return (
+    <>
+      <h1>Chai aur code</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}>Add Value</button>
+      <br/>
+      <button
+      onClick={removeValue}>Remove Value</button>
+    </> 
+  )
+}
+
+export default App
