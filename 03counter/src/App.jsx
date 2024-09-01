a37f7930ad5eae14b1fee5041a9d5496140da797
arijitar <arijit.roy155k@gmail.com>
2024-09-01 10:30:39 +0530

    Usestate

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     // counter = counter + 1
     if(counter < 20){
       setCounter(counter + 1 )
+      // setCounter((prevCounter)=> prevCounter + 1)
     }
     console.log("clicked", counter);
   
